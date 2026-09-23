bf74665eb54cfd61fab6794c6e9f2ce6f4b9cdc6
Sophiaaaaa382320 <philosophia.alfarabi123@gmail.com>
2026-09-23 14:50:33 +0700

    BattleSeason+MileStone

--- NewBattleSeason/Program.cs
+++ NewBattleSeason/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Input character's name: ");
+string name = Console.ReadLine();
+Console.Write("Input number of battles: ");
+int battles = int.Parse(Console.ReadLine());
+Console.Write("Input EXP gained per battle: ");
+int expPerBattle = int.Parse(Console.ReadLine());
+
+int totalExp = 0;
+Console.WriteLine("\n=== Battle Log ===");
+for (int i = 1; i <= battles; i++)
+{
+    totalExp += expPerBattle;
+
+    Console.WriteLine($"Battle {i} | EXP gained: {expPerBattle} | Total EXP: {totalExp}");
+    if (totalExp % 100 == 0)
+    {
+        Console.WriteLine($"*** Milestone reached! Total EXP: {totalExp} ***");
+    }
+}
+
+Console.WriteLine("\n=== BATTLE COMPLETE ===");
+Console.WriteLine($"Character : {name}");
+Console.WriteLine($"Battles   : {battles}");
+Console.WriteLine($"Total EXP : {totalExp}");
